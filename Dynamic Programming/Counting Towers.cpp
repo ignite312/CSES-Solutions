040d6357054f78132f7c8b0d42a1460c8d71cc6b
ignite312 <contact.emonkhan@gmail.com>
2024-03-17 04:50:24 +0600

    Dp section Complete!

--- Dynamic Programming/Counting Towers.cpp
+++ Dynamic Programming/Counting Towers.cpp
@@ -2,7 +2,45 @@
 Problem Name: Counting Towers
 Problem Link: https://cses.fi/problemset/task/2413/
 
-DP Category: 
+DP Category:
 Time Complexity:
-Resource:
+Resource: https://www.youtube.com/watch?v=MxhA0bY31wM
 */
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const int M = 1e9 + 7;
+const int N = 1e6 + 1;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int tt;
+    tt = 1;
+    // cin >> tt;
+    while (tt--) {
+        vector<vector<ll>> dp(N + 1, vector<ll>(8, 0));
+        for (int i = 0; i < 8; i++) {
+            dp[0][i] = 1;
+        }
+        for (int i = 1; i <= N; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (j == 0 || j == 1 || j == 4 || j == 5 || j == 6) {
+                    dp[i][j] = (dp[i - 1][0] + dp[i - 1][2] + dp[i - 1][4] +
+                                dp[i - 1][5] + dp[i - 1][6]) % M;
+                }
+                else {
+                    dp[i][j] = (dp[i - 1][1] + dp[i - 1][3] + dp[i - 1][7]) % M;
+                }
+            }
+        }
+        int q;
+        cin >> q;
+        while (q--) {
+            int n;
+            cin >> n;
+            cout << (dp[n - 1][3] + dp[n - 1][1]) % M << "\n";
+        }
+    }
+    return 0;
+}
--- README.md
+++ README.md
@@ -2,9 +2,10 @@
 
 This repository contains my accepted (AC) solutions to problems from the [CSES problem set](https://cses.fi/problemset/).
 
+## Table of Content
+- [Complet Dynamic Programming](https://github.com/ignite312/CSES-Solutions/tree/main/Dynamic%20Programming)
+- [Complete Tree Algorithms](https://github.com/ignite312/CSES-Solutions/tree/main/Tree%20Algorithms)
+
 ## Resources
 
 For those interested in competitive programming [Competitive Programmer’s Handbook](https://cses.fi/book/book.pdf) by Antti Laaksonen. The book offers comprehensive insights and strategies for tackling algorithmic problems.
-
-## Table of Content
-- Complete Tree Algorithm Section
