bc472106f07a110324913ce4388394144bae7e9e
ignite312 <contact.emonkhan@gmail.com>
2024-04-03 02:52:46 +0600

    Added a python script for Making CSES Editorial

--- GenerateFile.py
+++ GenerateFile.py
@@ -19,19 +19,18 @@ Resource:
     file_path = os.path.join(folder_path, file_name)
     with open(file_path, "w") as file:
         file.write(file_content)
-    print(f"File '{file_name}' created successfully in folder '{folder_name}'!")
+    # print(f"File '{file_name}' created successfully in folder '{folder_name}'!")
 
-if __name__ == "__main__":
-    try:
-        with open("data.txt", "r") as input_file:
-            for line in input_file:
-                elements = line.strip().split(",")
-                if len(elements) == 3:
-                    problem_name, problem_link, folder_name = elements
-                    create_cpp_file(problem_name, problem_link, folder_name)
-                else:
-                    print("Invalid format. Each line should contain three elements separated by commas.")
-    except FileNotFoundError:
-        print("data.txt file not found!")
-    except Exception as e:
-        print("An error occurred:", e)
+try:
+    with open("data.txt", "r") as input_file:
+        for line in input_file:
+            elements = line.strip().split(",")
+            if len(elements) == 3:
+                problem_name, problem_link, folder_name = elements
+                create_cpp_file(problem_name, problem_link, folder_name)
+            else:
+                print("Invalid format. Each line should contain three elements separated by commas.")
+except FileNotFoundError:
+    print("data.txt file not found!")
+except Exception as e:
+    print("An error occurred:", e)
