a7a69988cabf6d86c3505efcf43ac66005544ab2
ignite312 <contact.emonkhan@gmail.com>
2024-08-04 02:25:02 +0600

    String Section Complete!

--- Mathematics/Counting Coprime Pairs.cpp
+++ Mathematics/Counting Coprime Pairs.cpp
@@ -5,3 +5,58 @@ Idea:
 Complexity:
 Resource:
 */
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+const int N = 1000000;
+vector<int> d[N+1];
+ll valuesDivisibleBy[N+1], primeDivisors[N+1];
+
+void divisors() {
+    for(int i = 2; i <= N; i++) {
+        if(d[i].size())continue;
+        for(int j = i; j <= N; j+=i) {
+            d[j].push_back(i);
+        }
+    }
+}
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    divisors();
+    int tt;
+    tt = 1;
+    // cin >> tt;
+    while(tt--) {
+        int n;
+        cin >> n;
+        vector<int> v(n);
+        for(int i = 0; i < n; i++) {
+            cin >> v[i];
+        }
+        for(int i = 0; i < n; i++) {
+            for(int mask = 1; mask < (1 << d[v[i]].size()); mask++) {
+                int count = 0;
+                int number = 1;
+                for(int idx = 0; idx < d[v[i]].size(); idx++) {
+                    if(mask & (1 << idx)) {
+                        count++;
+                        number*=d[v[i]][idx];
+                    }
+                }
+                valuesDivisibleBy[number]++;
+                primeDivisors[number] = count;
+            }
+        }
+        ll ans = n*1LL*(n-1)/2;
+        for(int i = 2; i <= N; i++) {
+            if(primeDivisors[i] % 2 != 0) {
+                ans-=valuesDivisibleBy[i]*(valuesDivisibleBy[i]-1)/2;
+            }else {
+                ans+=valuesDivisibleBy[i]*(valuesDivisibleBy[i]-1)/2;
+            }
+        }
+        cout << ans << "\n";
+    }
+    return 0;
+}
--- String Algorithms/Required Substring.cpp
+++ String Algorithms/Required Substring.cpp
@@ -5,3 +5,71 @@ Idea:
 Complexity:
 Resource:
 */
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+const int M = 1e9 + 7;
+int n, m;
+/*
+dp[i][j] = Total number of strings of length i where the suffix of length j matches
+the prefix of the pattern string s.
+*/
+int dp[1001][101];
+/*
+mx_len[i][c] = the maximum length of the suffix of the string formed by 
+adding the character c to the prefix of length i of the given pattern s, 
+such that this suffix is also a prefix of s.
+*/
+int mx_len[100][26];
+
+int bigPow(ll b, ll p, const int M) {
+    int ans = 1;
+    b %= M;
+    if (b < 0) b += M;
+    while (p) {
+        if (p & 1) ans = ans * b % M;
+        b = b * b % M;
+        p >>= 1;
+    }
+    return ans;
+}
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int tt;
+    tt = 1;
+    // cin >> tt;
+    while(tt--) {
+        int n;
+        cin >> n;
+        string s;
+        cin >> s;
+        int m = (int)s.size();
+        for(int j = 0; j < m; j++) {
+          for(int c = 0; c < 26; c++) {
+            string pfx = s.substr(0, j);
+            pfx+=c+'A';
+            for(int k = 0; k < pfx.size(); k++) {
+              if(pfx.substr(k) == s.substr(0, pfx.size()-k)) {
+                mx_len[j][c] = max(mx_len[j][c], (int)pfx.size()-k);
+                break;
+              }
+            }
+          }
+        }
+        dp[0][0] = 1;
+        for(int i = 1; i <= n; i++) {
+          for(int j = 0; j < m; j++) {
+            for(int c = 0; c < 26; c++) {
+              dp[i][mx_len[j][c]] = (dp[i][mx_len[j][c]] + dp[i-1][j]) % M;
+            }
+          }
+        }
+        int ans = bigPow(26, n, M);
+        for(int j = 0; j < m; j++) {
+          ans = (ans  - dp[n][j] + M) % M;
+        }
+        cout << ans << "\n";
+    }
+    return 0;
+}
